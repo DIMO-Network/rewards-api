314c8821a17c78a6f3851b636f7f14ab8ec4f003
Allyson-English <allyson.english@gmail.com>
2023-01-19 21:49:19 -0700

    get average token distribution for current week

--- internal/api/rewards.go
+++ internal/api/rewards.go
@@ -30,6 +30,14 @@ type totalResp struct {
 	TotalPoints int64 `boil:"total_points"`
 }
 
+type totelTokens struct {
+	AverageTokens int64 `boil:"average_tokens"`
+}
+
+type maxWeek struct {
+	MaxWeek int64 `boil:"max_week"`
+}
+
 func (s *rewardsService) GetTotalPoints(ctx context.Context, _ *emptypb.Empty) (*pb.GetTotalPointsResponse, error) {
 	tp := new(totalResp)
 	query := models.NewQuery(
@@ -43,6 +51,34 @@ func (s *rewardsService) GetTotalPoints(ctx context.Context, _ *emptypb.Empty) (
 
 	out := &pb.GetTotalPointsResponse{
 		TotalPoints: tp.TotalPoints,
+	}
+	return out, nil
+}
+
+func (s *rewardsService) GetAverageTokens(ctx context.Context, _ *emptypb.Empty) (*pb.GetTotalPointsResponse, error) {
+	tt := new(totelTokens)
+	mw := new(maxWeek)
+	weekQuery := models.NewQuery(
+		qm.Select("max("+models.RewardColumns.IssuanceWeekID+") as max_week"),
+		qm.From(models.TableNames.Rewards),
+	)
+	if err := weekQuery.Bind(ctx, s.dbs().Reader, mw); err != nil {
+		s.logger.Err(err).Msg("Failed to get total points.")
+		return nil, status.Error(codes.Internal, "Internal error.")
+	}
+
+	tokenQuery := models.NewQuery(
+		qm.Select("sum("+models.RewardColumns.Tokens+") / count(distinct("+models.RewardColumns.UserDeviceTokenID+")) as total_tokens"),
+		qm.From(models.TableNames.Rewards),
+		qm.Where(models.RewardColumns.IssuanceWeekID+" = ", mw.MaxWeek),
+	)
+	if err := tokenQuery.Bind(ctx, s.dbs().Reader, tt); err != nil {
+		s.logger.Err(err).Msg("Failed to get total tokens.")
+		return nil, status.Error(codes.Internal, "Internal error.")
+	}
+
+	out := &pb.GetTotalPointsResponse{
+		TotalPoints: tt.AverageTokens,
 	}
 	return out, nil
 }
