3c656f95e48a17af2c18b8a44493b7c07ef7d693
Dylan Moreland <dylan@dimo.zone>
2023-02-22 19:41:43 -0500

    Descriptions

--- internal/controllers/transactions.go
+++ internal/controllers/transactions.go
@@ -50,11 +50,11 @@ func (r *RewardsController) GetTransactionHistory(c *fiber.Ctx) error {
 
 	for _, tx := range txes {
 		apiTx := APITransaction{
-			ChainID:     tx.ChainID,
-			Time:        tx.BlockTimestamp,
-			FromAddress: common.BytesToAddress(tx.AddressFrom),
-			ToAddress:   common.BytesToAddress(tx.AddressTo),
-			Value:       tx.Amount.Int(nil),
+			ChainID: tx.ChainID,
+			Time:    tx.BlockTimestamp,
+			From:    common.BytesToAddress(tx.AddressFrom),
+			To:      common.BytesToAddress(tx.AddressTo),
+			Value:   tx.Amount.Int(nil),
 		}
 		txHistory.Transactions = append(txHistory.Transactions, apiTx)
 	}
@@ -67,9 +67,16 @@ type TransactionHistory struct {
 }
 
 type APITransaction struct {
-	ChainID     int64          `json:"chainId" example:"137"`
-	Time        time.Time      `json:"time" example:"2023-01-22T09:00:12Z"`
-	ToAddress   common.Address `json:"to" example:"0xc66d80f5063677425270013136ef9fa2bf1f9f1a" swaggertype:"string"`
-	FromAddress common.Address `json:"from" example:"0xf316832fbfe49f90df09eee019c2ece87fad3fac" swaggertype:"string"`
-	Value       *big.Int       `json:"value" example:"10000000000000000" swaggertype:"number"`
+	// ChainID is the chain id of the chain on which the transaction took place. Important
+	// values are 137 for Polygon, 1 for Ethereum.
+	ChainID int64 `json:"chainId" example:"137"`
+	// Time is the timestamp of the block in which the transaction took place, in RFC-3999 format.
+	Time time.Time `json:"time" example:"2023-01-22T09:00:12Z"`
+	// From is the address of the source of the value, in 0x-prefixed hex.
+	From common.Address `json:"from" example:"0xf316832fbfe49f90df09eee019c2ece87fad3fac" swaggertype:"string"`
+	// To is the address of the recipient of the value, in 0x-prefixed hex.
+	To common.Address `json:"to" example:"0xc66d80f5063677425270013136ef9fa2bf1f9f1a" swaggertype:"string"`
+	// Value is the amount of token being transferred. Divide by 10^18 to get what people
+	// normally consider $DIMO.
+	Value *big.Int `json:"value" example:"10000000000000000" swaggertype:"number"`
 }
