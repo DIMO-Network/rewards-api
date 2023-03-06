5a63df014c91bae1c61920a08afa611c5b0a6bc3
Dylan Moreland <dylan@dimo.zone>
2023-03-06 12:16:03 -0500

    Cumulative sum

--- internal/controllers/balance.go
+++ internal/controllers/balance.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/DIMO-Network/rewards-api/models"
 	pb_users "github.com/DIMO-Network/shared/api/users"
+	"github.com/ericlagergren/decimal"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gofiber/fiber/v2"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/volatiletech/sqlboiler/v4/types"
 )
 
 // GetBalanceHistory godoc
@@ -41,6 +43,7 @@ func (r *RewardsController) GetBalanceHistory(c *fiber.Ctx) error {
 	// Terrible no good.
 	tfs, err := models.TokenTransfers(
 		qm.Where(models.TokenTransferTableColumns.AddressFrom+" != "+models.TokenTransferTableColumns.AddressTo),
+		models.TokenTransferWhere.Amount.NEQ(types.NewDecimal(decimal.New(0, 0))),
 		qm.Expr(
 			models.TokenTransferWhere.AddressTo.EQ(addr.Bytes()),
 			qm.Or2(models.TokenTransferWhere.AddressFrom.EQ(addr.Bytes())),
@@ -51,11 +54,20 @@ func (r *RewardsController) GetBalanceHistory(c *fiber.Ctx) error {
 		return err
 	}
 
+	runningBalance := big.NewInt(0)
+
 	for _, tf := range tfs {
+		delta := tf.Amount.Int(tf.Amount.Int(nil))
+		if common.BytesToAddress(tf.AddressFrom) == addr {
+			delta = new(big.Int).Mul(delta, big.NewInt(-1))
+		}
+
+		runningBalance = new(big.Int).Add(runningBalance, delta)
+
 		if len(balanceHistory.BalanceHistory) == 0 || balanceHistory.BalanceHistory[len(balanceHistory.BalanceHistory)-1].Time != tf.BlockTimestamp {
-			balanceHistory.BalanceHistory = append(balanceHistory.BalanceHistory, Balance{Time: tf.BlockTimestamp, Balance: tf.Amount.Int(nil)})
+			balanceHistory.BalanceHistory = append(balanceHistory.BalanceHistory, Balance{Time: tf.BlockTimestamp, Balance: runningBalance})
 		} else {
-			balanceHistory.BalanceHistory[len(balanceHistory.BalanceHistory)-1].Balance = new(big.Int).Add(balanceHistory.BalanceHistory[len(balanceHistory.BalanceHistory)-1].Balance, tf.Amount.Int(nil))
+			balanceHistory.BalanceHistory[len(balanceHistory.BalanceHistory)-1].Balance = new(big.Int).Add(balanceHistory.BalanceHistory[len(balanceHistory.BalanceHistory)-1].Balance, delta)
 		}
 	}
 
