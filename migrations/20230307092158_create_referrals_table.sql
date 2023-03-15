f84783f483bb31cca75db414df9b7f9b8edfa6dc
Dylan Moreland <dylan@dimo.zone>
2023-03-15 15:32:44 -0400

    Hit it with the formatter

--- migrations/20230307092158_create_referrals_table.sql
+++ migrations/20230307092158_create_referrals_table.sql
@@ -2,20 +2,17 @@
 -- +goose StatementBegin
 SET search_path = rewards_api, public;
 
-CREATE TYPE referrals_transfer_failure_reason AS ENUM ('ReferralInvalid', 'TxReverted');
+CREATE TYPE referrals_transfer_failure_reason AS ENUM (
+    'ReferralInvalid',
+    'TxReverted'
+);
 
-CREATE TABLE referrals(
-    referee bytea NOT NULL
-        CONSTRAINT referrals_referee_check CHECK (length(referee) = 20),
-    referrer bytea NOT NULL
-        CONSTRAINT referrals_referrer_check CHECK (length(referrer) = 20),
-    
-    transfer_successful BOOLEAN,
+CREATE TABLE referrals (
+    referee bytea NOT NULL CONSTRAINT referrals_referee_check CHECK (length(referee) = 20),
+    referrer bytea NOT NULL CONSTRAINT referrals_referrer_check CHECK (length(referrer) = 20),
+    transfer_successful boolean,
     transfer_failure_reason referrals_transfer_failure_reason,
-
-    request_id char(27) NOT NULL
-        CONSTRAINT referrals_request_id_fkey REFERENCES meta_transaction_requests(id),
-    
+    request_id char(27) NOT NULL CONSTRAINT referrals_request_id_fkey REFERENCES meta_transaction_requests (id),
     CONSTRAINT referrals_pkey PRIMARY KEY (referee, referrer)
 );
 -- +goose StatementEnd
@@ -25,4 +22,6 @@ CREATE TABLE referrals(
 SET search_path = rewards_api, public;
 
 DROP TABLE referrals;
+
+DROP TYPE referrals_transfer_failure_reason;
 -- +goose StatementEnd
