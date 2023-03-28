1f8d65242f5aa4e4adb2dc81ec9ca9a3f4425861
Allyson-English <allyson.english@gmail.com>
2023-03-28 08:48:09 -0600

    set referrals week_id default when creating column, then remove

--- migrations/20230321103141_add_issuance_week_to_referrals.sql
+++ migrations/20230321103141_add_issuance_week_to_referrals.sql
@@ -3,9 +3,12 @@
 SET search_path = rewards_api, public;
 
 ALTER TABLE referrals
-ADD COLUMN issuance_week_id int NOT NULL;
+ADD COLUMN issuance_week_id int NOT NULL DEFAULT 1;
+
+ALTER TABLE referrals ALTER COLUMN issuance_week_id DROP DEFAULT;
 
 ALTER TABLE referrals ADD CONSTRAINT referrals_issuance_week_id_fkey FOREIGN KEY (issuance_week_id) REFERENCES issuance_weeks(id);
+
 
 
 
