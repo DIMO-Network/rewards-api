725ac44cdaada9aa7aa565c0115e653b3a258abf
0xdev22 <doyin@dimo.zone>
2023-12-22 09:39:28 -0500

    Remove unused comment block and add missing env variables in settings template

--- internal/controllers/rewards.go
+++ internal/controllers/rewards.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"math/big"
 	"time"
 
@@ -14,13 +16,11 @@ import (
 	"github.com/DIMO-Network/rewards-api/models"
 	"github.com/DIMO-Network/shared/db"
 	pb_users "github.com/DIMO-Network/users-api/pkg/grpc"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/rs/zerolog"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"golang.org/x/exp/slices"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type RewardsController struct {
@@ -90,13 +90,11 @@ func (r *RewardsController) GetUserRewards(c *fiber.Ctx) error {
 	}
 
 	amMfrTokenToIntegration := make(map[uint64]*pb_defs.Integration)
-	swIntegrsByID := make(map[string]*pb_defs.Integration)
 	swIntegrsByTokenID := make(map[uint64]*pb_defs.Integration)
 
 	for _, intDesc := range allIntegrations.Integrations {
 		if intDesc.ManufacturerTokenId == 0 {
 			// Must be a software integration. Sort after this loop.
-			swIntegrsByID[intDesc.Id] = intDesc
 			swIntegrsByTokenID[intDesc.TokenId] = intDesc
 		} else {
 			// Must be the integration associated with a manufacturer.
@@ -187,28 +185,6 @@ func (r *RewardsController) GetUserRewards(c *fiber.Ctx) error {
 
 			outInts = append(outInts, uri)
 		}
-
-		// Take care of a software integration, if there is one.
-		/*for _, vehIntegr := range device.Integrations {
-			if integr, ok := swIntegrsByID[vehIntegr.Id]; ok {
-				uri := UserResponseIntegration{
-					ID:                   integr.Id,
-					Vendor:               integr.Vendor,
-					DataThisWeek:         false,
-					Points:               0,
-					OnChainPairingStatus: "NotApplicable",
-				}
-
-				if vehicleMinted && integSignalsThisWeek.Contains(integr.Id) {
-					uri.Points = int(integr.Points)
-					uri.DataThisWeek = true
-				}
-
-				outInts = append(outInts, uri)
-
-				break
-			}
-		}*/
 
 		rewards, err := models.Rewards(
 			models.RewardWhere.UserDeviceID.EQ(device.Id),
