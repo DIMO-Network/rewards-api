c70f578a1022830be9b9cd727eca76fbc362de2d
Dylan Moreland <dylan@dimo.zone>
2024-11-04 18:25:42 -0500

    Connection id variable. Will not scale

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -33,4 +33,5 @@ type Settings struct {
 	ContractEventTopic             string          `yaml:"CONTRACT_EVENT_TOPIC"`
 	DeviceDataIndexPrefix          string          `yaml:"DEVICE_DATA_INDEX_PREFIX"`
 	Clickhouse                     config.Settings `yaml:",inline"`
+	RuptelaConnectionID            string          `yaml:"RUPTELA_CONNECTION_ID"`
 }
