05b71feab478a73ce5652a5a08cef05e3aa6023f
Dylan Moreland <dylan@dimo.zone>
2024-12-15 20:34:51 -0500

    How'd this migration get in there?

--- migrations/20241212160245_week_point_token_conversion.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE issuance_weeks ADD COLUMN tokens_per_1k_points numeric(78);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE issuance_weeks DROP COLUMN tokens_per_1k_points;
--- +goose StatementEnd
